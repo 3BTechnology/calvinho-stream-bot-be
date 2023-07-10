9dc678721af009bbbbafdc4745e11ad7758669d0
Eric Oliveira <oiericoi@hotmail.co>
2023-07-10 12:47:36 -0300

    refactor: modify app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './modules/prisma';
+import { JwtService } from '@nestjs/jwt';
+import { AuthModule } from './common/auth/auth.module';
+import { AuthController } from './modules/auth/auth.controller';
+import { DiscordStrategy } from './modules/auth/strategies/discord.strategy';
+import { AuthService } from './modules/auth/auth.service';
+import { UserRepository } from './modules/user/user.repository';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, PrismaModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [
+    AppService,
+    JwtService,
+    DiscordStrategy,
+    AuthService,
+    UserRepository,
+  ],
 })
 export class AppModule {}
