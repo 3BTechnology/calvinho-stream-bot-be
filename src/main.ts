bb4463300c9f05bc4c012c8f89c61e85a501198a
Eric Oliveira <oiericoi@hotmail.co>
2023-07-10 12:48:05 -0300

    refactor: modify main file

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './modules/prisma/prisma.service';
+import * as cookieParse from 'cookie-parser';
+import { port } from './common/config/vars';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      allowedHeaders: ['content-type'],
+      origin: true,
+      credentials: true,
+    },
+  });
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
+  app.use(cookieParse());
+  await app.listen(port);
 }
 bootstrap();
