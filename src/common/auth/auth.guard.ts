81cc15ac87b75eb28d34c7fecb6b317fb7824333
Eric Oliveira <oiericoi@hotmail.co>
2023-07-10 12:40:40 -0300

    feat: add auth guard

---
+++ src/common/auth/auth.guard.ts
@@ -0,0 +1,34 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
+    const token = request.cookies.token;
+
+    if (token) {
+      try {
+        const decoded = await this.jwtService.verify(token);
+        if (decoded) {
+          return true;
+        }
+      } catch (error) {
+        throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+      }
+    }
+
+    response.clearCookie('jwt');
+
+    throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+  }
+}
