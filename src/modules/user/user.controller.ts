59d9281dc60e97c4f1948168f56d83210a1ebfe1
Eric Oliveira <oiericoi@hotmail.com>
2023-07-14 15:09:16 -0300

    refactor: modify user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from 'src/common/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -10,6 +20,13 @@ export class UserController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('me')
+  async me(@Req() req) {
+    const token = req.cookies['token'];
+    return this.userService.me(token);
   }
 
   @Get(':id')
