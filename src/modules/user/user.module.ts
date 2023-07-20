2b2087809763d93ecdd0c31c106e725513dcf6cb
Eric Oliveira <oiericoi@hotmail.com>
2023-07-20 11:29:39 -0300

    refactor: modify user module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,9 +4,18 @@ import { UserController } from './user.controller';
 import { PrismaService } from '../prisma';
 import { UserRepository } from './user.repository';
 import { JwtService } from '@nestjs/jwt';
+import { GuildService } from '../guild/guild.service';
+import { GuildRepository } from '../guild/guild.repository';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService, UserRepository, JwtService],
+  providers: [
+    UserService,
+    PrismaService,
+    UserRepository,
+    JwtService,
+    GuildService,
+    GuildRepository,
+  ],
 })
 export class UserModule {}
