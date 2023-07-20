d3515fd706c40d81d5409999f2e3984b1e9b89bf
Eric Oliveira <oiericoi@hotmail.com>
2023-07-20 11:29:13 -0300

    refactor: modify auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,21 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserRepository } from '../user/user.repository';
 import { Prisma, User } from '../prisma';
-import { Profile } from 'passport-discord';
+import Strategy, { Profile } from 'passport-discord';
 import { secret } from 'src/common/config/vars';
+import { GuildService } from '../guild/guild.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly userRepository: UserRepository,
+    private readonly guildService: GuildService,
   ) {}
 
   async findOrCreateUser(profile: Profile): Promise<{
     user: User;
     token: string;
   }> {
-    const { id, username, email, avatar } = profile;
+    const { id, username, email, avatar, guilds } = profile;
 
     const existingUser = await this.userRepository.findOneByEmail(email);
 
@@ -24,20 +26,26 @@ export class AuthService {
       return this.signIn(existingUser);
     }
 
-    return this.signUp({
-      discordId: id,
-      username,
-      email,
-      avatar,
-    });
+    return this.signUp(
+      {
+        discordId: id,
+        username,
+        email,
+        avatar,
+      },
+      guilds,
+    );
   }
 
-  private async signUp(user: {
-    discordId: string;
-    username: string;
-    email: string;
-    avatar: string;
-  }) {
+  private async signUp(
+    user: {
+      discordId: string;
+      username: string;
+      email: string;
+      avatar: string;
+    },
+    guilds: Strategy.GuildInfo[],
+  ) {
     const { discordId, username, email, avatar } = user;
 
     const input: Prisma.UserCreateInput = {
@@ -48,6 +56,17 @@ export class AuthService {
     };
 
     const newUser = await this.userRepository.create(input);
+
+    guilds.map(async (guild) => {
+      if (guild.owner === true) {
+        await this.guildService.create({
+          userId: newUser.id,
+          guildId: guild.id,
+          name: guild.name,
+          icon: guild.icon,
+        });
+      }
+    });
 
     const token = await this.generateToken(newUser.id, email);
 
